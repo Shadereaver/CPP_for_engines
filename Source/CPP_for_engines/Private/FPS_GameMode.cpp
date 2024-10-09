3869fba60ec979fc45e2cd5e6717d1870b78ddad
Shadereaver <117951478+Shadereaver@users.noreply.github.com>
2024-10-09 19:46:29 +0100

    custom gamemode

---
+++ Source/CPP_for_engines/Private/FPS_GameMode.cpp
@@ -0,0 +1,113 @@
+﻿#include "FPS_GameMode.h"
+
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+AActor* AFPS_GameMode::FindPlayerStart_Implementation(AController* Player, const FString& IncomingName)
+{
+	if (_PlayerStarts.Num() == 0)
+	{
+		TArray<AActor*> FoundActors;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), FoundActors);
+		for (AActor* Actor : FoundActors)
+		{
+			_PlayerStarts.Add(Actor);
+		}
+	}
+	if (_PlayerStarts.Num() > 0)
+	{
+		return _PlayerStarts[FMath::RandRange(0, _PlayerStarts.Num() - 1)];
+	}
+	return nullptr;
+}
+
+void AFPS_GameMode::PostLogin(APlayerController* NewPlayer)
+{
+	_Controllers.AddUnique(NewPlayer);
+	Super::PostLogin(NewPlayer);
+}
+
+void AFPS_GameMode::Logout(AController* Exiting)
+{
+	_Controllers.Remove(Exiting);
+	Super::Logout(Exiting);
+}
+
+void AFPS_GameMode::HandleMatchIsWaitingToStart()
+{
+	GetWorld()->GetTimerManager().SetTimer(_TimerDecreaseCountdown, this, &AFPS_GameMode::DecreaseCountdown, 1.f, false);
+	Super::HandleMatchIsWaitingToStart();
+}
+
+void AFPS_GameMode::HandleMatchHasStarted()
+{
+	DefaultPawnClass = _MatchPawn;
+	for(AController* Controller : _Controllers)
+	{
+		RestartPlayer(Controller);
+	}
+	Super::HandleMatchHasStarted();
+}
+
+void AFPS_GameMode::HandleMatchHasEnded()
+{
+	Super::HandleMatchHasEnded();
+}
+
+void AFPS_GameMode::OnMatchStateSet()
+{
+	FString Output;
+
+	if (MatchState == MatchState::WaitingToStart)
+	{
+		Output = "Waiting to start";
+	}
+	else if (MatchState == MatchState::InProgress)
+	{
+		Output = "Match in progress";
+	}
+	else if (MatchState == MatchState::WaitingPostMatch)
+	{
+		Output = "Waiting post match";
+	}
+	else if (MatchState == MatchState::EnteringMap)
+	{
+		Output = "Entering map";
+	}
+	else if (MatchState == MatchState::LeavingMap)
+	{
+		Output = "Leaving map";
+	}
+	else if (MatchState == MatchState::Aborted)
+	{
+		Output = "Match aborted";
+	}
+
+	UE_LOG(LogTemp, Display, TEXT("MATCH STATE CHANGED: %s"), *Output);
+	
+	Super::OnMatchStateSet();
+}
+
+bool AFPS_GameMode::ReadyToStartMatch_Implementation()
+{
+	return false;
+}
+
+bool AFPS_GameMode::ReadyToEndMatch_Implementation()
+{
+	return false;
+}
+
+void AFPS_GameMode::DecreaseCountdown()
+{
+	_CountdownTimer--;
+	UE_LOG(LogTemp, Display, TEXT("GAMEMODE COUNTDOWN: %d"), _CountdownTimer);
+	if (_CountdownTimer <= 0)
+	{
+		StartMatch();
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().SetTimer(_TimerDecreaseCountdown, this, &AFPS_GameMode::DecreaseCountdown, 1.f, false);
+	}
+}
---
+++ Source/CPP_for_engines/Private/FPS_GameMode.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "FPS_GameMode.generated.h"
+
+UCLASS(Abstract)
+class CPP_FOR_ENGINES_API AFPS_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual AActor* FindPlayerStart_Implementation(AController* Player, const FString& IncomingName) override;
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;
+	
+protected:
+	TArray<TObjectPtr<AActor>> _PlayerStarts;
+	FTimerHandle _TimerDecreaseCountdown;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<AController>> _Controllers;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Match management", meta=(ClampMin=1))
+	int _CountdownTimer = 1;
+	UPROPERTY(EditAnywhere, Category="Match management")
+	TSubclassOf<APawn> _MatchPawn;
+
+	virtual void HandleMatchIsWaitingToStart() override;
+	virtual void HandleMatchHasStarted() override;
+	virtual void HandleMatchHasEnded() override;
+	virtual void OnMatchStateSet() override;
+
+	virtual bool ReadyToStartMatch_Implementation() override;
+	virtual bool ReadyToEndMatch_Implementation() override;
+
+	UFUNCTION()
+	void DecreaseCountdown();
+};
